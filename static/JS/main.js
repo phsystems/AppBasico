db1e6df09851e990c4c1d476873add42a7668045
Pedro Henrique <ph.sistemasinformaco@gmail.com>
2019-03-29 19:23:00 -0400

    código sem alteração

---
+++ static/JS/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+
+function hideAllPages() {
+  $("#loginPage,#formPage,#mapPage").hide();
+}
+
+function showLoginPage() {
+  hideAllPages();
+  $("#loginPage").show();
+}
+
+function showFormPage() {
+  hideAllPages();
+  $("#formPage").show();
+  listForm();
+}
+
+function showMapPage() {
+  hideAllPages();
+  $("#mapPage").show();
+}
